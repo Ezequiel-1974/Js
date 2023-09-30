61f34da67acff717d0adf5075344a035589630fc
Ezequiel-1974 <guridiezequiel@gmail.com>
2023-09-30 14:45:29 -0300

    Haciendo correciones

--- Guridi_3raEntrega.js
+++ Guridi_3raEntrega.js
@@ -1,48 +1,61 @@
-let cartItems = [];
+let cart = [];
 
 function toggleProductList() {
     $('#product-list').toggle();
 }
 
 function addToCart(productName, productPrice) {
-    const item = { name: productName, price: productPrice, quantity: 1 };
-    cartItems.push(item);
+    const existingItem = cart.find(item => item.name === productName);
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.push({ name: productName, price: productPrice, quantity: 1 });
+    }
     updateCartDisplay();
 }
 
 function updateCartDisplay() {
     const cartItemsElement = document.getElementById('cart-items');
     const cartTotalElement = document.getElementById('cart-total');
+    const checkoutButton = document.getElementById('checkout-button');
     cartItemsElement.innerHTML = '';
 
     let totalPrice = 0;
 
-    for (const item of cartItems) {
+    for (const item of cart) {
         const li = document.createElement('li');
         li.className = 'product-item';
         li.innerHTML = `
             <span>${item.name} - $${item.price} x ${item.quantity}</span>
             <span>$${item.price * item.quantity}</span>
-            <button class="button is-danger" onclick="removeFromCart(${cartItems.indexOf(item)})">Eliminar</button>`;
+            <button class="button is-danger" onclick="removeFromCart('${item.name}')">Eliminar</button>`;
         cartItemsElement.appendChild(li);
         totalPrice += item.price * item.quantity;
     }
 
     cartTotalElement.textContent = totalPrice.toFixed(2);
+
+    // Habilitar o deshabilitar botón de pagar según si hay elementos en el carrito
+    checkoutButton.disabled = cart.length === 0;
 }
 
 function checkout() {
+    if (cart.length === 0) {
+        alert('El carrito está vacío. Agrega productos antes de pagar.');
+        return;
+    }
+
     alert('¡Gracias por su compra!');
-    cartItems = [];
+    cart = [];
     updateCartDisplay();
 }
 
-function removeFromCart(index) {
-    cartItems.splice(index, 1);
+function removeFromCart(productName) {
+    cart = cart.filter(item => item.name !== productName);
     updateCartDisplay();
 }
 
 function clearCart() {
-    cartItems = [];
+    cart = [];
     updateCartDisplay();
 }
